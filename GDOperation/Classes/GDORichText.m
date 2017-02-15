bfc89d8c3d71f99bb9c37c0ce297ea33ddc452ea
lumouren009 <diomo.2010@gmail.com>
2017-02-15 16:41:30 +0800

    embed space implementation

--- GDOperation/Classes/GDORichText.m
+++ GDOperation/Classes/GDORichText.m
@@ -190,14 +190,22 @@
     }
 
     if (op.hasInsertEmbed) {
-      unichar objectReplacementChar = 0xFFFC;
-      NSAttributedString * placeholder = [[NSAttributedString alloc] initWithString:[NSString stringWithCharacters:&objectReplacementChar length:1] attributes:@{NSFontAttributeName:[UIFont systemFontOfSize:0]}];
-      [self.attributedText insertAttributedString:placeholder atIndex:cursor];
-      float spacing = op.attributes.width.floatValue; // 间隔
-      [self.attributedText addAttribute:NSKernAttributeName
-                                  value:@(spacing)
-                                  range:NSMakeRange(cursor, 1)];
-      cursor += 1;
+      if (op.insertEmbed.space) {
+        float spacing = op.attributes.width.floatValue; // 间隔
+        if (spacing > 0) {
+          unichar objectReplacementChar = 0xFFFC;
+          NSAttributedString * placeholder = [[NSAttributedString alloc] initWithString:[NSString stringWithCharacters:&objectReplacementChar length:1] attributes:@{NSFontAttributeName:[UIFont systemFontOfSize:0]}];
+          [self.attributedText insertAttributedString:placeholder atIndex:cursor];
+          [self.attributedText addAttribute:NSKernAttributeName
+                                      value:@(spacing)
+                                      range:NSMakeRange(cursor, 1)];
+          cursor += 1;
+        }
+        
+      } else {
+        // other embed implementation
+        // cursor += 1;
+      }
       continue;
     }
   }
